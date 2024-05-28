1227f27f68b4d062659c20d2c07db7c7e0fa7a63
arafath-BS <arafath.santo@brainstation-23.com>
2024-05-28 19:56:34 +0600

    add header, footer, Static demo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css',]
 })
 export class HeaderComponent {
 
+
+  icon = faHeart;
+
+   headerText=
+   [
+    {name:'Home'},
+    {name:'Contact'},
+    {name:'About'},
+    {name:'Sign Up'},
+   ]
 }
