37b283de54e5c8f646f90f964c2703ae10359fa8
asifmahmud35 <asif.mahmud@brainstation-23.com>
2024-06-03 15:48:55 +0600

    adjust routing to all path

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -7,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit{
   demoProduct: number[] = Array(10).fill(1)
-  constructor() { }
+  constructor(private router:Router) { }
   ngOnInit(): void {
     // console.log(this.demoProduct);
   }
 
-
+  
 }
