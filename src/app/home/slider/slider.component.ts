4f33a72cbb7440b7f8e0d76136fa84ee42a5e3b0
arafath-BS <arafath.santo@brainstation-23.com>
2024-06-11 14:28:29 +0600

    home refactor, slider refactor, home resolver Refactor

--- src/app/home/slider/slider.component.ts
+++ src/app/home/slider/slider.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.css'
+  styleUrl: './slider.component.css',
 })
-export class SliderComponent {
-
-}
+export class SliderComponent {}
